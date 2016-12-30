e9e21a69fcbeb43d6fa91ea7c56480501c576b1d
Craig Morton <CraigMorton22@gmail.com>
2016-12-30 15:42:48 +0000

    Add tests to check board creates Cells as expected

--- specs/boardSpec.js
+++ specs/boardSpec.js
@@ -12,4 +12,18 @@ describe('Board', () => {
     assert.isArray(board.cells)
     assert.isArray(board.cells[0])
   })
+
+  it('should create first Cell with appropriate x and y values', () => {
+    const board = new Board({columns: 5, rows: 5})
+    const firstCell = board.cells[0][0]
+    assert.equal(1, firstCell.x)
+    assert.equal(5, firstCell.y)
+  })
+
+  it('should create last Cell with appropriate x and y values', () => {
+    const board = new Board({columns: 5, rows: 5})
+    const lastCell = board.cells[4][4]
+    assert.equal(5, lastCell.x)
+    assert.equal(1, lastCell.y)
+  })
 })
