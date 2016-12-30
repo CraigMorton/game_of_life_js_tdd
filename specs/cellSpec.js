691aac861c82d6f2832068a7c3c366b61b09c599
Craig Morton <CraigMorton22@gmail.com>
2016-12-30 14:43:22 +0000

    Add alive status to Cell

--- specs/cellSpec.js
+++ specs/cellSpec.js
@@ -12,4 +12,15 @@ describe('Cell', () => {
     assert.equal(5, cell.x)
     assert.equal(5, cell.y)
   })
+
+  it('should have alive status', () => {
+    assert.isBoolean((new Cell()).alive )
+  })
+
+  it('should be able to set alive status in constructor', () => {
+    const aliveCell = new Cell(1, 1, true)
+    const deadCell = new Cell(1, 2, false)
+    assert.equal(true, aliveCell.alive)
+    assert.equal(false, deadCell.alive)
+  })
 })
--- src/Cell.js
+++ src/Cell.js
@@ -1,6 +1,12 @@
-const Cell = function (x, y) {
+const randomBoolean = function () {
+  const randomNum = Math.floor(Math.random() * 2)
+  return (randomNum === 1)
+}
+
+const Cell = function (x, y, alive) {
   this.x = x
   this.y = y
+  this.alive = (alive != null) ? alive : randomBoolean()
 }
 
 export default Cell
