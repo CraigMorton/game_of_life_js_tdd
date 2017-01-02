809b605df50f47b79030cc195bdc7e149032f3bf
Craig Morton <CraigMorton22@gmail.com>
2017-01-02 15:26:50 +0000

    Game kills or revives Cells based on value / neighbour population

--- specs/gameSpec.js
+++ specs/gameSpec.js
@@ -1,6 +1,7 @@
 import {assert} from 'chai'
 import Game from '../src/Game.js'
 import Board from '../src/Board.js'
+import Cell from '../src/Cell.js'
 
 describe('Game', () => {
   
@@ -19,4 +20,54 @@ describe('Game', () => {
     assert.isObject(game.board)
   })
 
+  it('should kill cells with value <2', () => {
+    const cell = new Cell({x: 0, y: 0})
+    cell.value = 1
+    cell.alive = true
+    const game = new Game()
+    game.tickCell(cell)
+    assert.equal(false, cell.alive)
+  })
+
+  it('should kill cells with value >3', () => {
+    const cell = new Cell({x: 0, y: 0})
+    cell.value = 4
+    cell.alive = true
+    const game = new Game()
+    game.tickCell(cell)
+    assert.equal(false, cell.alive)
+  })
+
+  it('should revive cells with value 3', () => {
+    const cell = new Cell({x: 0, y: 0})
+    cell.value = 3
+    cell.alive = false
+    const game = new Game()
+    game.tickCell(cell)
+    assert.equal(true, cell.alive)
+  })
+
+  it('should leave cells with value 2-3 alive', () => {
+    const cell2 = new Cell({x: 2, y: 2})
+    const cell3 = new Cell({x: 3, y: 3})
+    cell2.value = 2
+    cell3.value = 3
+    cell2.alive = true
+    cell3.alive = true
+    const game = new Game()
+    game.tickCell(cell2)
+    game.tickCell(cell3)
+    assert.equal(true, cell2.alive)
+    assert.equal(true, cell3.alive)
+  })
+
+  it('should leave cells with value 2 dead', () => {
+    const cell = new Cell({x: 2, y: 2})
+    cell.value = 2
+    cell.alive = false
+    const game = new Game()
+    game.tickCell(cell)
+    assert.equal(false, cell.alive)
+  })
+  
 })
--- src/Game.js
+++ src/Game.js
@@ -3,4 +3,20 @@ const Game = function (board) {
   this.turns = 0
 }
 
+Game.prototype.tickCell = function (cell) {
+  if (cell.value == null) {
+    this.board.cellValue({x: cell.x, y: cell.y})
+  }
+  let value = cell.value
+  if (value >= 4) value = 4
+  if (value <= 1) value = 1
+  const isCellAlive = {
+    1: false,
+    2: cell.alive,
+    3: true,
+    4: false,
+  }
+  cell.alive = isCellAlive[value]
+}
+
 export default Game
